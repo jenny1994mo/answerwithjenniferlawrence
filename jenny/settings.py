558cc98839f9106a0f1060812d7a51018a2dc36b
nextn <kelechinwekes94@gmail.com>
2025-08-03 01:46:37 +0100

    push again

--- jenny/settings.py
+++ jenny/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'jenny.urls'
@@ -123,6 +124,9 @@ LOGIN_URL = '/login/'
 
 STATIC_URL = '/static/'
 STATIC_ROOT= os.path.join(BASE_DIR, "static")
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 #endstatic
 
